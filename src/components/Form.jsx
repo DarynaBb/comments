5c73ef49b20cda10a1d270a732b0de37c7a78f57
DarynaBb <darinabilenko@gmail.com>
2023-11-03 11:28:12 +0100

    +fetch

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,29 +5,51 @@ import { faComment } from '@fortawesome/free-solid-svg-icons'
 
 function Form() {
     const [user, setUser] = useState({name: "", comment:""});
-    const [userComments, setUserComments] = useState(
-        JSON.parse(localStorage.getItem("usersArray")) || []
-      );
-    const [counter, setCounter] = useState(0);
+    const [userComments, setUserComments] = useState(JSON.parse(localStorage.getItem("usersArray")) || [] );
+    
+    const [isChecked, setIsChecked] = useState(false);
+    const [comments, setComments] = useState([]);
+    const [counter, setCounter] = useState(comments.length);
+
 
     useEffect(() => {
         localStorage.setItem('usersArray', JSON.stringify(userComments));
-        setCounter(userComments.length);
+        setCounter(userComments.length + comments.length);
       }, [userComments]);
 
-
+      useEffect(() => {
+       
+        const fetchData = async () => {
+            try {
+              const response = await fetch("https://corsproxy.io/?https://tiny-house-helden.de/api/comments.json");
+              if (response.ok) {
+                const data = await response.json();
+                setComments(data); 
+              } else {
+                console.warn("Response not okay");
+              }
+            } catch (error) {
+              console.error("An error occurred:", error);
+            }
+          };
+        fetchData();  
+    }, [])
+  
     const onChangeHandler = (event) => {
         setUser({...user, [event.target.name]: event.target.value});
     }
     
     const onClickHandler = (event) => {
-        const newUser = user;
-        console.log(newUser)
         event.preventDefault();
+        if (isChecked) {
+            const newUser = user;
+        console.log(newUser)
+        
         if (newUser.comment) {
             setUserComments([...userComments, newUser]);
         }
         setUser({name:"", comment:""})
+        } 
     }
 
     const removeComment = (comment) => {
@@ -35,7 +57,15 @@ function Form() {
             const updatedUserComments = prevComments.filter(userComment => userComment.comment !== comment )
             return updatedUserComments;
         })  
+        setComments(prevComments => {
+            const updatedComments = prevComments.filter(userComment => userComment.text !== comment )
+            return updatedComments;
+        }) 
         counter > 0 ? setCounter(prevValue => prevValue -1) : setCounter(0);
+    }
+
+    const onCklickInput = () => {
+        isChecked ? setIsChecked(false) : setIsChecked (true);
     }
     
   return (
@@ -44,8 +74,17 @@ function Form() {
         <div>
             <p className='my-[20px] text-center'><FontAwesomeIcon icon={faComment} /> {counter} Kommentaren </p>
             <ul className='flex justify-center flex-wrap gap-[10px]'>
+                {comments.map((comment) => (
+                    <li key={comment.id} className='basis-3/12 my-[10px] border-2 rounded-[5px] p-[15px] card flex flex-col justify-center w-[50%] shrink'>
+                        <div className='p-[10px]'>
+                            <p><span className='font-semibold'>Name:</span>  {comment.name}</p>
+                            <p><span className='font-semibold'>Commentar:</span> {comment.text}</p>
+                        </div>
+                        <button onClick={() => removeComment(comment.text)} className='btn border-[1px] rounded-[8px] p-[5px] cursor-pointer mt-[10px] text-sm'>Kommentar löschen</button>
+                    </li>
+                ))}
                 {userComments.map((userComment, index) => (
-                    <li key={index} className='my-[10px] border-2 rounded-[5px] p-[15px] card flex flex-col justify-center'>
+                    <li key={index} className='basis-3/12 my-[10px] border-2 rounded-[5px] p-[15px] card flex flex-col justify-center'>
                         <div className='p-[10px]'>
                             <p><span className='font-semibold'>Name:</span>  {userComment.name}</p>
                             <p><span className='font-semibold'>Commentar:</span> {userComment.comment}</p>
@@ -71,8 +110,8 @@ function Form() {
                 <label htmlFor="check">Meinen Namen, E-Mail und Website in diesem Browser speichern, bis ich wieder kommentiere</label> 
             </div>
             <div>
-                <input className='mr-[5px]' type="checkbox" />
-                <label htmlFor="check">Ich habe die Datenschutzerklärung gelesen und akzeptiere sie. *</label> 
+                <input className='mr-[5px]' type="checkbox" onClick={onCklickInput}/>
+                <label className={isChecked ? "" : "bg-red-500"} htmlFor="check">Ich habe die Datenschutzerklärung gelesen und akzeptiere sie. *</label> 
             </div>
 
             <button onClick={onClickHandler} className='my-[30px] border-[1px] rounded-[10px] py-[10px] px-[15px]'>Kommentar abschicken</button>
